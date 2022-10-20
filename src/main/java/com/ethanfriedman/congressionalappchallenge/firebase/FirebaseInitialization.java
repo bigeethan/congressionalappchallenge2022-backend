5916508c0b2c5674ceb02549fe9a1cd1810db13c
unknown <ethan.sean.friedman@gmail.com>
2022-10-19 21:00:22 -0400

    fifth

--- src/main/java/com/ethanfriedman/congressionalappchallenge/firebase/FirebaseInitialization.java
+++ src/main/java/com/ethanfriedman/congressionalappchallenge/firebase/FirebaseInitialization.java
@@ -3,6 +3,7 @@ package com.ethanfriedman.congressionalappchallenge.firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -12,13 +13,12 @@ import java.io.FileInputStream;
 public class FirebaseInitialization {
     @PostConstruct
     public void initilization() {
-        FileInputStream serviceAccount =
-                null;
         try {
-            serviceAccount = new FileInputStream("C:\\Users\\Ethan Friedman\\Desktop\\congressionalappchallenge (1)\\appchallenge-5dbdf-firebase-adminsdk-1ymu0-33cb0387eb.json");
+            ClassPathResource serviceAccount =
+                    new ClassPathResource("appchallenge-5dbdf-firebase-adminsdk-1ymu0-33cb0387eb.json");
 
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
                     .build();
 
             FirebaseApp.initializeApp(options);
