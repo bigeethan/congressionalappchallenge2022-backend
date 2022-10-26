3f0e99bc7283879507db7b0d07a594300e5e4b01
unknown <ethan.sean.friedman@gmail.com>
2022-10-25 21:20:46 -0400

    cors fixed

--- src/main/java/com/ethanfriedman/congressionalappchallenge/CongressionalappchallengeApplication.java
+++ src/main/java/com/ethanfriedman/congressionalappchallenge/CongressionalappchallengeApplication.java
@@ -18,16 +18,12 @@ public class CongressionalappchallengeApplication {
 	}
 
 	@Bean
-	public FilterRegistrationBean corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-		bean.setOrder(0);
-		return bean;
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOriginPatterns("https://*.domain.com");
+			}
+		};
 	}
 }
